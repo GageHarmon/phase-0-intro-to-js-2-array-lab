22dd17a6f43aeef1b2ab56879c7ac47dea7dfad9
Gage Harmon <gaharmon1@gmail.com>
2023-01-22 17:52:43 -0700

    commit tree 2c14aa9d15c00fca59d1724d53d33d8e28dfeb9e

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat (Ruth) {
+    cats.push(Ruth)
+    return cats;
+}
+
+function destructivelyPrependCat (JoJo) {
+    cats.unshift(JoJo)
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+  }
+  
+  function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+  }
+  
+  function appendCat(Ellie){
+    let newCats= [...cats,Ellie];
+    return newCats;
+  }
+  
+  function prependCat(Dash){
+    let moreNewCats=[Dash, ...cats];
+    return moreNewCats
+  }
+  
+  function removeFirstCat(){
+    var cats2= cats.slice(1)
+    return cats2
+  }
+
+  function removeLastCat(){
+    let cats3= cats.slice(0,cats.length-1);
+    return cats3;
+  }
